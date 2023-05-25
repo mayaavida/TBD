d6bbeda21e120a05975ff93044931f96a9de0811
Maya Peters <mayampeters@gmail.com>
2023-05-24 20:32:51 -0500

    set up pool

---
+++ .bash_profile
@@ -0,0 +1,1 @@
+DB_PASSWORD="v2_44iHH_yyP3b6EtxhYmghyELYvvUq8"
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const pg = require('pg');
+
 const PORT = process.env.PORT || 8000;
+const DB_PASSWORD = process.env.DB_PASSWORD
 
 app.listen(PORT, () => {
     console.log('listening on port: ', PORT);
@@ -8,8 +11,29 @@ app.listen(PORT, () => {
 
 app.use(express.json());
 
+const pool = new pg.Pool({
+    host: 'db.bit.io',
+    port: 5432,
+    ssl: true,
+    database: 'mayaavida/trial',
+    user: 'mayaavida',
+    password: DB_PASSWORD
+});
 
-app.get('/message', (req, res) => {
-    console.log('Request for message was made');
+
+app.get('/', (req, res) => {
     res.sendStatus(202);
-})
+});
+
+app.get('/messages', (req, res) => {
+    let query = 'SELECT * FROM "messages";';
+
+    pool.query(query)
+    .then((result) => {
+        res.status(200).send(result.rows);
+    })
+    .catch((error) => {
+        console.error(error);
+        res.sendStatus(500);
+    });
+});
